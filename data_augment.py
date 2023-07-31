bd78e183fa10c8ef8af0ec9286401b90c7c1712d
jiwon <jiwon3401@gmail.com>
2023-07-31 15:48:26 +0900

    augment

---
+++ data_augment.py
@@ -0,0 +1,8 @@
+from tools.dataset import pack_dataset_to_hdf5
+from tools.audio_augment import augment_raw_audio
+from loguru import logger
+
+if __name__ == '__main__':
+    logger.info('Creating Clotho augmentation files...')
+    augment_raw_audio(dataset, train_meta_dict)
+    logger.info('Creating raw audio DONE!')
