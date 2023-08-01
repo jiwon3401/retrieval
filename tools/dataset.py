75394206438adadfe7eaf211f1935096dabcf518
jiwon <jiwon3401@gmail.com>
2023-08-01 17:17:04 +0900

    update

--- tools/dataset.py
+++ tools/dataset.py
@@ -10,9 +10,8 @@ from pathlib import Path
 from tqdm import tqdm
 from tools.file_io import load_csv_file, write_pickle_file
 
-from tools.audio_augment import spec_augment
+#from tools.audio_augment import spec_augment
 #from audio_augmentations import *
-
 
 
 
@@ -132,119 +131,6 @@ def pack_dataset_to_hdf5(dataset):
     write_pickle_file(words_list, 'data/{}/pickles/words_list.p'.format(dataset))
 
 
-def pack_augment_dataset_to_hdf5(dataset):
-
-    splits = ['train', 'val', 'test']
-    sampling_rate = 32000
-    all_captions = [] 
-    #csv file에 있는 caption들을 list로 한번에 저장 ex) train data -> 3839*5 = 19195개 caption 저장
-    
-    #if dataset == 'AudioCaps':
-    #    audio_duration = 10
-    if dataset == 'Clotho':
-        audio_duration = 30
-        
-    else:
-        raise NotImplementedError(f'No dataset named: {dataset}')
-
-    max_audio_length = audio_duration * sampling_rate # 30 * 32000
-    
-    
-    for split in splits:
-        csv_path = 'data/{}/csv_files/{}.csv'.format(dataset, split)
-        audio_dir = 'data/{}/waveforms/{}/'.format(dataset, split)
-        hdf5_path = 'data/{}/hdf5s/{}/'.format(dataset, split)
-
-        # make dir for hdf5
-        Path(hdf5_path).mkdir(parents=True, exist_ok=True)
-
-        meta_dict = load_metadata(dataset, csv_path)
-        # meta_dict: {'audio_names': [], 'captions': []}
-
-        audio_nums = len(meta_dict['audio_name'])
-
-        if split == 'train':
-            # store all captions in training set into a list
-            if dataset == 'Clotho':
-                for caps in meta_dict['captions']:
-                    for cap in caps:
-                        all_captions.append(cap)
-            else:
-                all_captions.extend(meta_dict['captions'])
-
-        start_time = time.time()
-
-        ### file name = train_augment.h5
-        # augment는 train set에서만 적용시켜야하니까 코드 나눠서 만들기 
-
-
-        if split=='train':
-            with h5py.File(hdf5_path+'{}_augment.h5'.format(split), 'w') as hf:
-                hf.create_dataset('audio_name', shape=(audio_nums,), dtype=h5py.special_dtype(vlen=str))
-                hf.create_dataset('audio_length', shape=(audio_nums,), dtype=np.uint32)
-                hf.create_dataset('waveform', shape=(audio_nums, max_audio_length), dtype=np.float32)
-
-                if split == 'train' and dataset == 'AudioCaps':
-                    hf.create_dataset('caption', shape=(audio_nums,), dtype=h5py.special_dtype(vlen=str))
-                
-                else: #'clotho dataset'
-                    hf.create_dataset('caption', shape=(audio_nums, 5), dtype=h5py.special_dtype(vlen=str))
-
-                for i in tqdm(range(audio_nums)):
-                    audio_name = meta_dict['audio_name'][i]
-
-                    audio, _ = librosa.load(audio_dir + audio_name, sr=sampling_rate, mono=True)
-                    audio, audio_length = pad_or_truncate(audio, max_audio_length)
-
-                    #augmentation
-
-
-
-
-                    hf['audio_name'][i] = audio_name.encode()
-                    hf['audio_length'][i] = audio_length
-                    hf['waveform'][i] = audio
-                    hf['caption'][i] = meta_dict['captions'][i]
-
-
-        else:
-            with h5py.File(hdf5_path+'{}_augment.h5'.format(split), 'w') as hf:
-                # file name contains 'augment' but does not implement augmentation b/c these are not the training data.
-                # just for separating h5 file name with not augmented file.
-
-                hf.create_dataset('audio_name', shape=(audio_nums,), dtype=h5py.special_dtype(vlen=str))
-                hf.create_dataset('audio_length', shape=(audio_nums,), dtype=np.uint32)
-                hf.create_dataset('waveform', shape=(audio_nums, max_audio_length), dtype=np.float32)
-
-                if split == 'train' and dataset == 'AudioCaps':
-                    hf.create_dataset('caption', shape=(audio_nums,), dtype=h5py.special_dtype(vlen=str))
-                
-                else: #'clotho dataset'
-                    hf.create_dataset('caption', shape=(audio_nums, 5), dtype=h5py.special_dtype(vlen=str))
-
-                for i in tqdm(range(audio_nums)):
-                    audio_name = meta_dict['audio_name'][i]
-
-                    audio, _ = librosa.load(audio_dir + audio_name, sr=sampling_rate, mono=True)
-                    audio, audio_length = pad_or_truncate(audio, max_audio_length)
-
-
-                    hf['audio_name'][i] = audio_name.encode()
-                    hf['audio_length'][i] = audio_length
-                    hf['waveform'][i] = audio
-                    hf['caption'][i] = meta_dict['captions'][i]
-
-        logger.info(f'Packed {split} set to {hdf5_path} using {time.time() - start_time} s.')
-    
-    words_list, words_freq = _create_vocabulary(all_captions)
-    # 총 4368개
-    # words_dictionary = {words_list[i]: words_freq[i] for i in range(len(words_freq))}
-    # print(words_dictionary)
-
-    
-    logger.info(f'Creating vocabulary: {len(words_list)} tokens!')
-    write_pickle_file(words_list, 'data/{}/pickles/augment_words_list.p'.format(dataset))
-
 
 def _create_vocabulary(captions):
     
@@ -289,3 +175,4 @@ def pad_or_truncate(x, audio_length):
     else:
         return x[:audio_length], audio_length
 
+    
